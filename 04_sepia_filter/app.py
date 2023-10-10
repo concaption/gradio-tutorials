ade938c1f2f56affcd22f219d24ddff695e91025
Usama Navid <concaption@gmail.com>
2023-10-10 12:47:01 +0000

    Quick Commit

---
+++ 04_sepia_filter/app.py
@@ -0,0 +1,21 @@
+"""
+Sepia Filter
+"""
+import numpy as np
+import gradio as gr
+
+def sepia(input_img):
+    """
+    Takes in an image and returns the sepia version of that image.
+    """
+    sepia_filter = np.array([
+        [0.393, 0.769, 0.189],
+        [0.349, 0.686, 0.168],
+        [0.272, 0.534, 0.131]
+    ])
+    sepia_img = input_img.dot(sepia_filter.T)
+    sepia_img /= sepia_img.max()
+    return sepia_img
+
+demo = gr.Interface(sepia, gr.Image(shape=(200, 200)), "image")
+demo.launch()
--- Makefile
+++ Makefile
@@ -4,3 +4,11 @@ setup:
 install:
 	pip install --upgrade pip &&\
 		pip install -r requirements.txt
+run-01:
+	gradio 01_hello_world_1/app.py
+run-02:
+	gradio 02_hello_world_2/app.py
+run-03:
+	gradio 03_hello_world_3/app.py
+run-04:
+	gradio 04_sepia_filter/app.py
